864b3df273a29ff4a02c4ca68c0be34b2c782cfc
Robin Brooks <sapricotzine@gmail.com>
2019-09-16 14:12:16 -0400

    copied gradle stuff from toodoose

---
+++ build/resources/main/public/javascripts/client.js
@@ -0,0 +1,21 @@
+class Application extends React.Component {
+    render() {
+        return (
+            <div>
+                <PlusButton className="plus-at-the-top"/>
+                <Header/>
+                <ItemList/>
+                <PlusButton className="plus-at-the-bottom"/>
+            </div>
+        );
+    }
+}
+
+const Header = () => (
+    <header>
+        <h1>TODOOSE Robin Brooks</h1>
+        <p><small>A <a href="https://github.com/jhu-oose/todoose">to-do application</a> for <a href="https://www.jhu-oose.com">OOSE</a></small></p>
+    </header>
+);
+
+ReactDOM.render(<Application/>, document.querySelector("#application"));
