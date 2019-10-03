53a807e7219f767bbff813d9b670520dcf09da00
Robin Brooks <sapricotzine@gmail.com>
2019-10-03 08:58:23 -0400

    lessonlist => courselist; index.html => client.js

--- src/main/resources/public/javascripts/client.js
+++ src/main/resources/public/javascripts/client.js
@@ -1,21 +1,23 @@
-class Application extends React.Component {
-    render() {
-        return (
-            <div>
-                <PlusButton className="plus-at-the-top"/>
-                <Header/>
-                <ItemList/>
-                <PlusButton className="plus-at-the-bottom"/>
-            </div>
-        );
-    }
-}
-
-const Header = () => (
-    <header>
-        <h1>MONADIC</h1>
-        <p><small>A <a href="https://github.com/jhu-oose/2019-group-monadic">monad tutorial application</a> for <a href="https://www.monadic.com">OOSE</a></small></p>
-    </header>
-);
-
-ReactDOM.render(<Application/>, document.querySelector("#application"));
+const getDataFromServer = async () => {
+    const response = await fetch("/courses");
+    const dataFromServer = await response.json()
+    ReactDOM.render(
+        <div>
+            <h1>Monadic</h1>
+            <ul>
+                {
+                    dataFromServer.map(course => <li key={course.id}>{<button onClick={() => console.log("hello world")}>Try course: {course.id}</button>}</li>)
+                }
+            </ul>
+            <button>next</button>
+        </div>,
+        document.querySelector("#application")
+    );
+    //we can't be like todoose and let people update lessons by sending
+    //requests to the server, but later we will want to update people's
+    //answer boxes every so often so we should still have this
+    window.setTimeout(() => {getDataFromServer(); }, 200);
+    //also note: button should be fetch click () => arrow fuction too keep it from
+    //running every 200 ms
+};
+getDataFromServer();
