44f98138d90340027b7a2e5ca6a6847af20ef338
Robin Brooks <sapricotzine@gmail.com>
2019-10-03 10:15:00 -0400

    angry this.props.course

--- src/main/resources/public/javascripts/client.js
+++ src/main/resources/public/javascripts/client.js
@@ -1,18 +1,51 @@
 const getDataFromServer = async () => {
     const response = await fetch("/courses");
-    const dataFromServer = await response.json()
+    const dataFromServer = await response.json();
+    const Header = () => <h1>Monadic</h1>
+    const NextButton = () => <button>next</button>
+
+    class CourseList extends React.Component {
+        render() {
+            return <ul>{dataFromServer.map(course => <Course course={course}/>)}</ul>;
+        }
+    }
+
+    class Course extends React.Component {
+        render() {
+            return (
+                <li key={this.props.course.id}> {
+                    <form>
+                        {console.log("in course")}
+                        <button onClick={() =>
+                            console.log("hello world")}>
+                            Try course: {this.props.course.id}
+                        </button>
+                        <p>
+                            {console.log(this.props.course.id)}
+                        </p>
+                    </form>
+                } </li>
+            );
+        }
+    }
+
+
+    class Application extends React.Component {
+        render() {
+            return (
+                <div>
+                    <Header/>
+                    <CourseList/>
+                    <NextButton/>
+                </div>
+            )
+        }
+    }
+
     ReactDOM.render(
-        <div>
-            <h1>Monadic</h1>
-            <ul>
-                {
-                    dataFromServer.map(course => <li key={course.id}>{<button onClick={() => console.log("hello world")}>Try course: {course.id}</button>}</li>)
-                }
-            </ul>
-            <button>next</button>
-        </div>,
+        <Application/>,
         document.querySelector("#application")
-    );
+    )
     //we can't be like todoose and let people update lessons by sending
     //requests to the server, but later we will want to update people's
     //answer boxes every so often so we should still have this
