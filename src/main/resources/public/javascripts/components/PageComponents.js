a8943608a77e5f486e61ae4ddd0b327f644b9dd6
Andrew Hellinger <hellinger04@gmail.com>
2019-10-14 15:13:13 -0400

    page now changes on click of a button but doesn't display list of lessons on new page

--- src/main/resources/public/javascripts/components/PageComponents.js
+++ src/main/resources/public/javascripts/components/PageComponents.js
@@ -81,43 +81,39 @@ class CourseButton extends React.Component {
     render() {
         return (
             <div>
-            <li key={this.props.course.id}> {
-                <form>
-                    <button onClick={() => {this.props.changePage("course", this.props.course.id)} }>Try course: {this.props.course.id}</button>
-                </form>
-            } </li>
+                <li key={this.props.course.id}> {
+                    <form>
+                        <button onClick={() => {this.props.changePage("course", this.props.course.id)} }>Try course: {this.props.course.id}</button>
+                    </form>
+                } </li>
             </div>
         );
     }
 }
 
 class CourseList extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { courses: [], curr: 0};
-    // }
-    //
-    // async getDataFromServer() {
-    //     // TODO get what course we are on from the server
-    //     this.setState({ courses: await (await fetch("/courses")).json() });
-    //     window.setTimeout(() => { this.getDataFromServer(); }, 1000);
-    // }
-    //
+    constructor(props) {
+        super(props);
+        this.state = { courses: [], curr: 0};
+    }
+
+    async getDataFromServer() {
+        // TODO get what course we are on from the server
+        this.setState({ courses: await (await fetch("/courses")).json() });
+        window.setTimeout(() => { this.getDataFromServer(); }, 1000);
+    }
+
     componentDidMount() {
-        // this.getDataFromServer();
-        console.log("in componentDidMount");
+        this.getDataFromServer();
     }
 
     render() {
-        // console.log(this.state.courses);
-        // const cc = this.state.courses.filter(course => course.id === this.state.curr);
-        console.log("test");
-        // return (
-        //     <div>
-        //         <GoToNext onClick={() => this.setState({curr: this.state.curr+1})}/>
-        //         <ul>{this.state.courses[0].lessonlist.lessonElements.map(lelement => <LessonElement key={lelement.id} lelement={lelement}/>)}</ul>
-        //     </div>
-        // )
+        return (
+            <div>
+                <GoToNext onClick={() => this.setState({curr: this.state.curr+1})}/>
+                <ul>{this.state.courses.map(course => <CourseButton key={course.id} course={course} changePage={this.props.changePage}/>)}</ul>
+            </div>
+        )
     }
 }
 
@@ -136,26 +132,20 @@ class Monadic extends React.Component {
 
 
     render() {
-        //console.log(this.state)
-        return (
-            <div>
-                <CourseList changePage={this.changePage}/>
-            </div>
-        );
-
-        // if (this.state.page === "courselist") {
-        //     return (
-        //         <div>
-        //             <CourseList changePage={this.changePage}/>
-        //         </div>
-        //     );
-        // } else if (this.state.page === "course") {
-        //     return (
-        //         <div>
-        //             this is a test
-        //         </div>
-        //     );
-        // }
+        console.log(this.state)
+        if (this.state.page === "courselist") {
+            return (
+                <div>
+                    <CourseList changePage={this.changePage}/>
+                </div>
+            );
+        } else if (this.state.page === "course") {
+            return (
+                <div>
+                    this is a test
+                </div>
+            );
+        }
     }
 
 }
