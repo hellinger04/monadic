bd74131e4fefdc2f5e78e802f6435959f97831eb
NathanV09 <nathanvallapu@gmail.com>
2019-10-20 23:50:13 -0400

    post, put, delete database methods

--- src/main/java/com/jhuoose/monadic/Server.java
+++ src/main/java/com/jhuoose/monadic/Server.java
@@ -1,14 +1,18 @@
 package com.jhuoose.monadic;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jhuoose.monadic.models.Course;
 import com.jhuoose.monadic.models.Lesson;
 import com.jhuoose.monadic.models.LessonElement;
 import com.jhuoose.monadic.models.Text;
 import io.javalin.Javalin;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         //construct ArrayList to hold Course lessons
         var courseZeroLessons = new ArrayList<Lesson>();
@@ -47,9 +51,56 @@ public class Server {
         courseList.add(firstCourse);
         courseList.add(secondCourse);
 
-        Javalin app = Javalin.create(config -> { config.addStaticFiles("/public"); });
+        ObjectMapper mapper = new ObjectMapper();
+        Javalin app = Javalin.create(config -> {
+            config.addStaticFiles("/public");
+        });
+        var connection = DriverManager.getConnection("jdbc:sqlite:monadic.db");
+        app.events(event -> {
+            event.serverStarting(() -> {
+                var statement = connection.createStatement();
+                statement.execute("CREATE TABLE IF NOT EXISTS courses (identifier INTEGER PRIMARY KEY AUTOINCREMENT, lessons VARCHAR)");
+                statement.close();
+            });
+            event.serverStopped(() -> {
+                connection.close();
+            });
+        });
         app.get("/courses", ctx -> {
             ctx.json(courseList);
+        });
+
+        app.post("/courses", ctx -> {
+            // put empty lesson list into the course. we will use update() to add lessons later.
+            ArrayList<Lesson> lessons = new ArrayList<>();
+            String JSONLessons = mapper.writeValueAsString(lessons);
+            var createStatement = connection.createStatement();
+            createStatement.execute("INSERT INTO courses (lessons) VALUES " + JSONLessons);
+            createStatement.close();
+            ctx.status(201);
+        });
+
+        app.delete("/items/:identifier", ctx -> {
+            var statement = connection.prepareStatement("DELETE FROM courses WHERE identifier = ?");
+            statement.setInt(1, Integer.parseInt(ctx.pathParam("identifier")));
+            if (statement.executeUpdate() == 0) {
+                ctx.status(404);
+            } else {
+                ctx.status(204);
+            }
+            statement.close();
+        });
+
+        app.put("/items/:identifier", ctx -> {
+            var statement = connection.prepareStatement("UPDATE courses SET lessons = ? WHERE identifier = ?");
+            statement.setString(1, ctx.formParam("lessons", ""));
+            statement.setInt(2, Integer.parseInt(ctx.pathParam("identifier")));
+            if (statement.executeUpdate() == 0) {
+                ctx.status(404);
+            } else {
+                ctx.status(204);
+            }
+            statement.close();
         });
 
         app.start(System.getenv("PORT") == null ? 7000 : Integer.parseInt(System.getenv("PORT")));
