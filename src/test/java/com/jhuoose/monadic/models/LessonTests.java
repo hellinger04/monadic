df6152a922a93348708e0957290742bb56ffaf5d
Andrew Hellinger <hellinger04@gmail.com>
2019-11-02 14:08:47 -0400

    added comments

--- src/test/java/com/jhuoose/monadic/models/LessonTests.java
+++ src/test/java/com/jhuoose/monadic/models/LessonTests.java
@@ -14,17 +14,23 @@ class LessonTests {
     @Test
     void testLessonConstructor() {
         Lesson lesson = new Lesson(0, 3);
-        assertEquals(3, lesson.getID());
 
-        LessonElement expectedText = new Text(1, "\n\nWrite a function `foldFunctional` that takes in three arguments:\n" +
-                "\n" + "- A two argument function\n" +
-                "- An accumulator, which can be of any value (though it has to make sense for the function and the List)\n" +
-                "- A `List`\n" + "\n" +
-                "Your `foldFunctional` will run the input function repeatedly over the accumulator and each element of the List, and will return the final value of the accumulator as a result. For example, if `adder` is your function, `0` is your accumulator, and `[1, 2, 3]` is your list, you should end up with the number `6` as your output (since 1 + 2 + 3 = 6).\n" +
-                "\n" +
-                "You should not mutate the value of any pre-existing accumulator; you can only create new accumulator instances. You also can't mutate your `List` since it's an Immutable.js object. Furthermore, you should not use for or while loops; use recursion only.\n" +
-                "\n" + "**Problem 0.2.1:** Create a function `filterFunctional` that takes two arguments:\n\n");
+        // retrieve actual text from Lesson and set expected text
         LessonElement actualText = lesson.getLessonElements().get(2);
+        LessonElement expectedText = new Text(1, "\n\nWrite a function `foldFunctional` that takes in three "
+                + "arguments:\n" + "\n" + "- A two argument function\n" + "- An accumulator, which can be of any value "
+                + "(though it has to make sense for the function and the List)\n" + "- A `List`\n\n" +
+                "Your `foldFunctional` will run the input function repeatedly over the accumulator and each element of "
+                + "the List, and will return the final value of the accumulator as a result. For example, if `adder` is"
+                + " your function, `0` is your accumulator, and `[1, 2, 3]` is your list, you should end up with the "
+                + "number `6` as your output (since 1 + 2 + 3 = 6).\n\n" + "You should not mutate the value of any "
+                + "pre-existing accumulator; you can only create new accumulator instances. You also can't mutate your "
+                + "`List` since it's an Immutable.js object. Furthermore, you should not use for or while loops; use "
+                + "recursion only.\n\n" + "**Problem 0.2.1:** Create a function `filterFunctional` that takes two "
+                + "arguments:\n\n");
+
+        // assert that values from Lesson are as expected
+        assertEquals(3, lesson.getID());
         assertEquals(actualText.getContents(), expectedText.getContents());
     }
 
@@ -49,15 +55,15 @@ class LessonTests {
         LessonElement text = course.getLessonList().get(3).getLessonElements().get(4);
 
         // set expected values for problem and text
-        String expectedProblem = "function adder(x, y) {\n" +
-                "    // your code here\n" +
-                "    // solution added for development purposes\n" +
-                "    return x + y\n" + "}\n\n";
+        String expectedProblem = "function adder(x, y) {\n" + "    // your code here\n" + "    // solution added for " +
+                "development purposes\n" + "    return x + y\n" + "}\n\n";
         String expectedText = "\n\n" +
                 "- A predicate, ie. a function that takes in a single argument and returns true or false.\n" +
-                "- A `List`\n" + "\n" +
-                "Your `filterFunctional` will run the predicate repeatedly over each element in the list. The output is a new `List` where only elements that make the predicate return `true` exist. For instance, if `greaterThan2` is your predicate and `[1, 3, 2, 5, 7]` is your `List`, then `filterFunctional` should return `[3, 5, 7]`.\n" +
-                "\n" + "Again, you should not mutate the `List`, nor should you use for or while loops.\n";
+                "- A `List`\n\n" + "Your `filterFunctional` will run the predicate repeatedly over each element in the "
+                + "list. The output is a new `List` where only elements that make the predicate return `true` exist. "
+                + "For instance, if `greaterThan2` is your predicate and `[1, 3, 2, 5, 7]` is your `List`, then " +
+                "`filterFunctional` should return `[3, 5, 7]`.\n\n" + "Again, you should not mutate the `List`, nor " +
+                "should you use for or while loops.\n";
 
         // assert that values from Course are as expected
         assertEquals(1, course.getID());
@@ -67,14 +73,17 @@ class LessonTests {
 
     @Test
     void testTextConstructor() {
+        // construct new Text and assert values are as expected
         LessonElement text = new Text(1, "test text");
         assertEquals(1, text.getID());
         assertEquals("test text", text.getContents());
+        // Text should not be a Problem, so assert this is the case
         assertFalse(text.isProblem());
     }
 
     @Test
     void testTestCaseConstructor() {
+        // construct new TestCase and assert values are as expected
         TestCase testCase = new TestCase(3,"input", "output");
         assertEquals(3, testCase.getID());
         assertEquals("input", testCase.getInput());
@@ -83,31 +92,27 @@ class LessonTests {
 
     @Test
     void testProblemConstructor() {
-        LessonElement problem = new Problem(5, "function adder(x, y) {\n" +
-                "    // Your code here\n" +
-                "}\n" +
-                "\n" +
-                "// TESTS\n" +
-                "// adder(2, 3) ==> 5\n" +
-                "// adder(100, -100) ==> 0\n" +
-                "// adder(\"Foo\", \"Bar\") ==> \"FooBar\"");
+        // construct new Problem by providing sample text, define expected contents based on sample text
+        LessonElement problem = new Problem(5, "function adder(x, y) {\n" + "    // Your code here\n" + "}\n\n"
+                + "// TESTS\n" + "// adder(2, 3) ==> 5\n" + "// adder(100, -100) ==> 0\n" + "// adder(\"Foo\", \"Bar\")"
+                + " ==> \"FooBar\"");
+        String expectedContents = "function adder(x, y) {\n" + "    // Your code here\n" + "}\n" + "\n";
+
+        // assert values are as expected
         assertEquals(5, problem.getID());
         assertTrue(problem.isProblem());
-        assertEquals("function adder(x, y) {\n" + "    // Your code here\n" + "}\n" + "\n", problem.getContents());
+        assertEquals(expectedContents, problem.getContents());
     }
 
     @Test
     void testProblemTestCases() {
-        LessonElement problem = new Problem(5, "function adder(x, y) {\n" +
-                "    // Your code here\n" +
-                "}\n" +
-                "\n" +
-                "// TESTS\n" +
-                "// adder(2, 3) ==> 5\n" +
-                "// adder(100, -100) ==> 0\n" +
-                "// adder(\"Foo\", \"Bar\") ==> \"FooBar\"");
+        // construct new Problem by providing sample text, get list of tests from Problem
+        Problem problem = new Problem(5, "function adder(x, y) {\n" + "    // Your code here\n" + "}\n\n"
+                + "// TESTS\n" + "// adder(2, 3) ==> 5\n" + "// adder(100, -100) " + "==> 0\n// adder(\"Foo\", \"Bar\")"
+                + " ==> \"FooBar\"");
+        List<TestCase> tests = problem.getTests();
 
-        List<TestCase> tests = ((Problem) problem).getTests();
+        // assert test values are as expected
         assertEquals("adder(2, 3)", tests.get(0).getInput());
         assertEquals("adder(100, -100)", tests.get(1).getInput());
         assertEquals("adder(\"Foo\", \"Bar\")", tests.get(2).getInput());
