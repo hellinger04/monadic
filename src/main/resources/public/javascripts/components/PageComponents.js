1ef41ecc459058f6bf3a5a93b9daeba3b6e7aac0
kjone183 <kjone183@jhu.edu>
2019-11-03 17:16:58 -0500

    Add line numbers for codemirror boxes (for more advanced grader debugging in the future possibly)

--- src/main/resources/public/javascripts/components/PageComponents.js
+++ src/main/resources/public/javascripts/components/PageComponents.js
@@ -90,7 +90,8 @@ class Problem extends React.Component {
     componentDidMount() {
        this.mirror = CodeMirror.fromTextArea(document.getElementById('problem' + this.props.element.id), {
            mode: "javascript",
-           theme: "solarized"
+           theme: "solarized",
+           lineNumbers: true
        });
     }
 
